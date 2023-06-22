35afe683317b16d19e5e762b2b3f57e73371f5d9
Rohit Patil <rohitvpatil0810@gmail.com>
2023-06-22 11:08:25 +0530

    Attemp 6 to deploy

--- controllers/society/society.controller.js
+++ controllers/society/society.controller.js
@@ -116,9 +116,7 @@ module.exports.society_signup = async (req, res) => {
 
 module.exports.getRegisteredSocieties = async (req, res) => {
   try {
-    const societies = await Society.find({ is_approved: true }).populate(
-      "society_id"
-    );
+    const societies = await Society.find({ is_approved: true });
 
     const modifiedSocieties = await Promise.all(
       societies.map(async (society) => {
