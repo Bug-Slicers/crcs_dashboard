128bdc75598d5cfbefe79946cc3b7734e2f45596
Rohit Patil <rohitvpatil0810@gmail.com>
2023-06-22 10:19:25 +0530

    Attempt 2 to deploy

--- routers/index.js
+++ routers/index.js
@@ -2,29 +2,32 @@ const { Router } = require("express");
 const societyRouter = require("./society/society.router");
 const adminRouter = require("./admin/admin.router");
 const applicationRouter = require("./application/application.router");
-const { downloadFile, deleteFile } = require("../controllers/file/file.controller");
+const {
+  downloadFile,
+  deleteFile,
+} = require("../controllers/file/file.controller");
 const router = Router();
 
-router.get("/", (req, res) => {
-    res.status(202).json({
-        success: true,
-        message: "Welcome to the backend of CRCS Dashboard",
-        team: ["Rohit , Manas"],
-        organization: "Bug-Slicers"
-    });
-});
+// router.get("/", (req, res) => {
+//     res.status(202).json({
+//         success: true,
+//         message: "Welcome to the backend of CRCS Dashboard",
+//         team: ["Rohit , Manas"],
+//         organization: "Bug-Slicers"
+//     });
+// });
 
 router.post("/logout", (req, res) => {
-    res
-        .clearCookie("jwt")
-        .status(204)
-        .json({ message: "Logged out successfully" });
-})
+  res
+    .clearCookie("jwt")
+    .status(204)
+    .json({ message: "Logged out successfully" });
+});
 
-router.get('/download/:folder/:filename', downloadFile);
-router.post("/deletefile/:id/:filename", deleteFile)
+router.get("/download/:folder/:filename", downloadFile);
+router.post("/deletefile/:id/:filename", deleteFile);
 
 router.use("/societies", societyRouter);
 router.use("/admin", adminRouter);
 router.use("/applications", applicationRouter);
-module.exports = router
+module.exports = router;
